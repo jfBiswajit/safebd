65fb7c1068b41c2eb0b6c901ec47e8132a238186
Biswajit Biswas <jfbiswajit@gmail.com>
2020-07-09 12:46:15 +0600

    Add: Bootsrtap starter template

--- app/Http/Controllers/SafeBdMainController.php
+++ app/Http/Controllers/SafeBdMainController.php
@@ -6,7 +6,10 @@ use Illuminate\Http\Request;
 
 class SafeBdMainController extends Controller
 {
-    public function index() {
-        return view('index');
-    }
+	public function index()
+	{
+		return view('index', [
+			'title' => 'Online Shopping For Health Care in Bangladesh'
+		]);
+	}
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,2 +1,3 @@
 @extends('layouts.header')
+
 @extends('layouts.footer')
