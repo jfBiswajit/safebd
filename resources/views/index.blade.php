024391579632dbb84e5b2f7ea5e1fc4f2588b7bc
Biswajit Biswas <jfbiswajit@gmail.com>
2020-07-10 08:31:38 +0600

    Add: Header added

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,34 @@
 @extends('layouts.header')
-
+<div class="container">
+	<header class="p-4">
+		<div class="row">
+			<div class="col-lg-3">
+				<div class="logo">
+					<h1 class="font-Galada text-success">SafeBD.Net</h1>
+				</div>
+			</div>
+			<div class="col-lg-5">
+				<form class="form-inline">
+					<input type="text" name="" id="" class="form-control mr-2" placeholder="What are you looking for?">
+					<button type="submit" class="btn btn-success">Search</button>
+				</form>
+			</div>
+			<div class="col-lg-4">
+				<div class="d-flex">
+					<ion-icon name="call-outline" size="large" class="mr-2"></ion-icon>
+					<span class="log-reg">
+						<span><b>Hotline</b></span>
+						<span>+88 01842181614</span>
+					</span>
+					<ion-icon name="cart-outline" size="large" class="ml-4"></ion-icon>
+					<ion-icon name="person-outline" size="large" class="mr-2 ml-4"></ion-icon>
+					<span class="log-reg">
+						<span><a href=""><b>Log In</b></a></span>
+						<span><a href=""><b>Register</b></a></span>
+					</span>
+				</div>
+			</div>
+		</div>
+	</header>
+</div>
 @extends('layouts.footer')
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,13 +1,22 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+<head>
+	{{-- Required meta tags --}}
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title>{{ $title }}</title>
-  </head>
-  <body>
+	{{-- Bootstrap CSS --}}
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+		integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+	{{-- Custom goole fonts --}}
+	<link href="https://fonts.googleapis.com/css2?family=Galada&family=Noto+Sans+JP&display=swap" rel="stylesheet">
+
+	{{-- Custom CSS Style --}}
+	<link rel="stylesheet" href="{{ asset('css/main.css') }}">
+
+	<title>{{ $title }}</title>
+</head>
+
+<body>
