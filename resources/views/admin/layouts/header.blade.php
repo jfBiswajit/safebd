94bb4b583d858f38162dc0d689b4728cf245358f
Biswajit Biswas <jfbiswajit@gmail.com>
2020-07-11 22:32:39 +0600

    Add: Login page added

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -49,58 +49,35 @@
 			<nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
 				<div class="sb-sidenav-menu">
 					<div class="nav">
-						<div class="sb-sidenav-menu-heading">Core</div>
-						<a class="nav-link" href="index.html">
+						<a class="nav-link active" href="index.html">
 							<div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
 							Dashboard
 						</a>
-						<div class="sb-sidenav-menu-heading">Interface</div>
-						<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseLayouts"
-							aria-expanded="false" aria-controls="collapseLayouts">
+
+						<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#orders"
+							aria-expanded="false" aria-controls="orders">
 							<div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
-							Layouts
+							Orders
 							<div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
 						</a>
-						<div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-parent="#sidenavAccordion">
+						<div class="collapse" id="orders" aria-labelledby="headingOne" data-parent="#sidenavAccordion">
 							<nav class="sb-sidenav-menu-nested nav">
-								<a class="nav-link" href="layout-static.html">Static Navigation</a>
-								<a class="nav-link" href="layout-sidenav-light.html">Light Sidenav</a>
+								<a class="nav-link" href="layout-static.html">Pending</a>
+								<a class="nav-link" href="layout-sidenav-light.html">Completed</a>
 							</nav>
 						</div>
-						<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
-							aria-expanded="false" aria-controls="collapsePages">
-							<div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
-							Pages
+
+						<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#products"
+							aria-expanded="false" aria-controls="products">
+							<div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+							Products
 							<div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
 						</a>
-						<div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-parent="#sidenavAccordion">
-							<nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
-								<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#pagesCollapseAuth"
-									aria-expanded="false" aria-controls="pagesCollapseAuth">
-									Authentication
-									<div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-								</a>
-								<div class="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne"
-									data-parent="#sidenavAccordionPages">
-									<nav class="sb-sidenav-menu-nested nav">
-										<a class="nav-link" href="login.html">Login</a>
-										<a class="nav-link" href="register.html">Register</a>
-										<a class="nav-link" href="password.html">Forgot Password</a>
-									</nav>
-								</div>
-								<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#pagesCollapseError"
-									aria-expanded="false" aria-controls="pagesCollapseError">
-									Error
-									<div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-								</a>
-								<div class="collapse" id="pagesCollapseError" aria-labelledby="headingOne"
-									data-parent="#sidenavAccordionPages">
-									<nav class="sb-sidenav-menu-nested nav">
-										<a class="nav-link" href="401.html">401 Page</a>
-										<a class="nav-link" href="404.html">404 Page</a>
-										<a class="nav-link" href="500.html">500 Page</a>
-									</nav>
-								</div>
+						<div class="collapse" id="products" aria-labelledby="headingOne" data-parent="#sidenavAccordion">
+							<nav class="sb-sidenav-menu-nested nav">
+								<a class="nav-link" href="layout-static.html">Add New</a>
+								<a class="nav-link" href="layout-sidenav-light.html">Update</a>
+								<a class="nav-link" href="layout-sidenav-light.html">Delete</a>
 							</nav>
 						</div>
 						<div class="sb-sidenav-menu-heading">Addons</div>
@@ -120,5 +97,5 @@
 				</div>
 			</nav>
 		</div>
-            <div id="layoutSidenav_content">
-            	<main>
+		<div id="layoutSidenav_content">
+			<main>
