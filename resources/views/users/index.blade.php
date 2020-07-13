3d9ff5f760615b2bf13cfca88aa79139aeaa9209
Biswajit Biswas <jfbiswajit@gmail.com>
2020-07-13 13:50:44 +0600

    Add: Home page fully completed

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -65,58 +65,18 @@
 	</div>
 	<div class="p-4">
 		<div class="row">
-			<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-				<div class="card-deck">
-					<div class="card">
-						<img class="card-img-top" src="https://www.zatista.ie/sitemap/email-images/300x300.jpg"
-							alt="Card image cap">
-						<div class="card-body">
-							<h5 class="card-title font-weight-light">Product Title</h5>
-							<h5 class="float-right text-danger"><b>৳ 50</b></h5>
-							<a href="#" class="btn btn-success">Add To Cart</a>
-						</div>
+			@for ($i = 0; $i < 4; $i++) <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+				<div class="card">
+					<img class="card-img-top" src="https://www.zatista.ie/sitemap/email-images/300x300.jpg" alt="Card image cap">
+					<div class="card-body">
+						<p class="card-title text-capitalize" style="font-size: 0.9rem">AirDoctor Digital Thermometer</p>
+						<h5 class="float-right text-danger" style="font-size: 0.9rem"><b>৳ 250</b></h5>
+						<button href="#" class="btn btn-success add_to_cart font-weight-light" data-id="10"
+							data-name="AirDoctor Digital Thermometer" data-price="40">Add To Cart</button>
 					</div>
 				</div>
-			</div>
-			<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-				<div class="card-deck">
-					<div class="card">
-						<img class="card-img-top" src="https://www.zatista.ie/sitemap/email-images/300x300.jpg"
-							alt="Card image cap">
-						<div class="card-body">
-							<h5 class="card-title font-weight-light">Product Title</h5>
-							<h5 class="float-right text-danger"><b>৳ 50</b></h5>
-							<a href="#" class="btn btn-success">Add To Cart</a>
-						</div>
-					</div>
 				</div>
-			</div>
-			<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-				<div class="card-deck">
-					<div class="card">
-						<img class="card-img-top" src="https://www.zatista.ie/sitemap/email-images/300x300.jpg"
-							alt="Card image cap">
-						<div class="card-body">
-							<h5 class="card-title font-weight-light">Product Title</h5>
-							<h5 class="float-right text-danger"><b>৳ 50</b></h5>
-							<a href="#" class="btn btn-success">Add To Cart</a>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-				<div class="card-deck">
-					<div class="card">
-						<img class="card-img-top" src="https://www.zatista.ie/sitemap/email-images/300x300.jpg"
-							alt="Card image cap">
-						<div class="card-body">
-							<h5 class="card-title font-weight-light">Product Title</h5>
-							<h5 class="float-right text-danger"><b>৳ 50</b></h5>
-							<a href="#" class="btn btn-success">Add To Cart</a>
-						</div>
-					</div>
-				</div>
-			</div>
+				@endfor
 		</div>
 	</div>
 </section>
@@ -134,58 +94,18 @@
 	</div>
 	<div class="p-4">
 		<div class="row">
-			<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-				<div class="card-deck">
-					<div class="card">
-						<img class="card-img-top" src="https://www.zatista.ie/sitemap/email-images/300x300.jpg"
-							alt="Card image cap">
-						<div class="card-body">
-							<h5 class="card-title font-weight-light">Product Title</h5>
-							<h5 class="float-right text-danger"><b>৳ 50</b></h5>
-							<a href="#" class="btn btn-success">Add To Cart</a>
-						</div>
+			@for ($i = 0; $i < 4; $i++) <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+				<div class="card">
+					<img class="card-img-top" src="https://www.zatista.ie/sitemap/email-images/300x300.jpg" alt="Card image cap">
+					<div class="card-body">
+						<p class="card-title text-capitalize" style="font-size: 0.9rem">AirDoctor Digital Thermometer</p>
+						<h5 class="float-right text-danger" style="font-size: 0.9rem"><b>৳ 250</b></h5>
+						<button href="#" class="btn btn-success add_to_cart font-weight-light" data-id="10"
+							data-name="AirDoctor Digital Thermometer" data-price="40">Add To Cart</button>
 					</div>
 				</div>
-			</div>
-			<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-				<div class="card-deck">
-					<div class="card">
-						<img class="card-img-top" src="https://www.zatista.ie/sitemap/email-images/300x300.jpg"
-							alt="Card image cap">
-						<div class="card-body">
-							<h5 class="card-title font-weight-light">Product Title</h5>
-							<h5 class="float-right text-danger"><b>৳ 50</b></h5>
-							<a href="#" class="btn btn-success">Add To Cart</a>
-						</div>
-					</div>
 				</div>
-			</div>
-			<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-				<div class="card-deck">
-					<div class="card">
-						<img class="card-img-top" src="https://www.zatista.ie/sitemap/email-images/300x300.jpg"
-							alt="Card image cap">
-						<div class="card-body">
-							<h5 class="card-title font-weight-light">Product Title</h5>
-							<h5 class="float-right text-danger"><b>৳ 50</b></h5>
-							<a href="#" class="btn btn-success">Add To Cart</a>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-				<div class="card-deck">
-					<div class="card">
-						<img class="card-img-top" src="https://www.zatista.ie/sitemap/email-images/300x300.jpg"
-							alt="Card image cap">
-						<div class="card-body">
-							<h5 class="card-title font-weight-light">Product Title</h5>
-							<h5 class="float-right text-danger"><b>৳ 50</b></h5>
-							<a href="#" class="btn btn-success">Add To Cart</a>
-						</div>
-					</div>
-				</div>
-			</div>
+				@endfor
 		</div>
 	</div>
 </section>
@@ -203,58 +123,18 @@
 	</div>
 	<div class="p-4">
 		<div class="row">
-			<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-				<div class="card-deck">
-					<div class="card">
-						<img class="card-img-top" src="https://www.zatista.ie/sitemap/email-images/300x300.jpg"
-							alt="Card image cap">
-						<div class="card-body">
-							<h5 class="card-title font-weight-light">Product Title</h5>
-							<h5 class="float-right text-danger"><b>৳ 50</b></h5>
-							<a href="#" class="btn btn-success">Add To Cart</a>
-						</div>
+			@for ($i = 0; $i < 4; $i++) <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+				<div class="card">
+					<img class="card-img-top" src="https://www.zatista.ie/sitemap/email-images/300x300.jpg" alt="Card image cap">
+					<div class="card-body">
+						<p class="card-title text-capitalize" style="font-size: 0.9rem">AirDoctor Digital Thermometer</p>
+						<h5 class="float-right text-danger" style="font-size: 0.9rem"><b>৳ 250</b></h5>
+						<button href="#" class="btn btn-success add_to_cart font-weight-light" data-id="10"
+							data-name="AirDoctor Digital Thermometer" data-price="40">Add To Cart</button>
 					</div>
 				</div>
-			</div>
-			<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-				<div class="card-deck">
-					<div class="card">
-						<img class="card-img-top" src="https://www.zatista.ie/sitemap/email-images/300x300.jpg"
-							alt="Card image cap">
-						<div class="card-body">
-							<h5 class="card-title font-weight-light">Product Title</h5>
-							<h5 class="float-right text-danger"><b>৳ 50</b></h5>
-							<a href="#" class="btn btn-success">Add To Cart</a>
-						</div>
-					</div>
 				</div>
-			</div>
-			<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-				<div class="card-deck">
-					<div class="card">
-						<img class="card-img-top" src="https://www.zatista.ie/sitemap/email-images/300x300.jpg"
-							alt="Card image cap">
-						<div class="card-body">
-							<h5 class="card-title font-weight-light">Product Title</h5>
-							<h5 class="float-right text-danger"><b>৳ 50</b></h5>
-							<a href="#" class="btn btn-success">Add To Cart</a>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-				<div class="card-deck">
-					<div class="card">
-						<img class="card-img-top" src="https://www.zatista.ie/sitemap/email-images/300x300.jpg"
-							alt="Card image cap">
-						<div class="card-body">
-							<h5 class="card-title font-weight-light">Product Title</h5>
-							<h5 class="float-right text-danger"><b>৳ 50</b></h5>
-							<a href="#" class="btn btn-success">Add To Cart</a>
-						</div>
-					</div>
-				</div>
-			</div>
+				@endfor
 		</div>
 	</div>
 </section>
