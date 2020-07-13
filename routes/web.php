73b5f62633142006a1fedcb7ba9fdc196a141074
Biswajit Biswas <jfbiswajit@gmail.com>
2020-07-13 16:23:31 +0600

    Add: Route groupe added

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'SafeBdMainController@index');
 Route::get('/product_details', 'SafeBdMainController@ProductDetails');
 Route::get('/cart', 'SafeBdMainController@Cart');
+Route::post('/add_to_cart', 'SafeBdMainController@AddToCart');
 Route::get('/checkout', 'SafeBdMainController@Checkout');
 Route::post('/thank_you', 'SafeBdMainController@Thankyou');
 
@@ -14,14 +15,13 @@ Route::get('/pending_orders', 'SafeBdMainController@PendingOrder');
 Route::get('/completed_orders', 'SafeBdMainController@CompletedOrder');
 Route::get('/add_new_product', 'SafeBdMainController@AddNewProduct');
 
-Route::post('/add_to_cart', 'SafeBdMainController@AddToCart');
-
-
-Route::get('/safety', 'SafeBdMainController@Safety');
-Route::get('/fire', 'SafeBdMainController@Fire');
-Route::get('/lab', 'SafeBdMainController@Lab');
-Route::get('/electronics', 'SafeBdMainController@Electronics');
-Route::get('/mechanical', 'SafeBdMainController@Mechanical');
-Route::get('/cevil', 'SafeBdMainController@Cevil');
-Route::get('/textile', 'SafeBdMainController@Textile');
-Route::get('/islamic', 'SafeBdMainController@Islamic');
+Route::prefix('category')->group(function () {
+  Route::get('/safety', 'SafeBdMainController@Safety');
+  Route::get('/fire', 'SafeBdMainController@Fire');
+  Route::get('/lab', 'SafeBdMainController@Lab');
+  Route::get('/electronics', 'SafeBdMainController@Electronics');
+  Route::get('/mechanical', 'SafeBdMainController@Mechanical');
+  Route::get('/cevil', 'SafeBdMainController@Cevil');
+  Route::get('/textile', 'SafeBdMainController@Textile');
+  Route::get('/islamic', 'SafeBdMainController@Islamic');
+});
