0af4069108496382b0f84b589d62f8080d87d224
Biswajit Biswas <jfbiswajit@gmail.com>
2020-07-13 17:32:26 +0600

    Add: Route group added

--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,13 @@ Route::post('/add_to_cart', 'SafeBdMainController@AddToCart');
 Route::get('/checkout', 'SafeBdMainController@Checkout');
 Route::post('/thank_you', 'SafeBdMainController@Thankyou');
 
-Route::get('/dashboard', 'SafeBdMainController@Dashboard');
-Route::get('/login', 'SafeBdMainController@Login');
-Route::get('/pending_orders', 'SafeBdMainController@PendingOrder');
-Route::get('/completed_orders', 'SafeBdMainController@CompletedOrder');
-Route::get('/add_new_product', 'SafeBdMainController@AddNewProduct');
+Route::prefix('admin')->group(function () {
+  Route::get('/dashboard', 'SafeBdMainController@Dashboard');
+  Route::get('/login', 'SafeBdMainController@Login');
+  Route::get('/pending_orders', 'SafeBdMainController@PendingOrder');
+  Route::get('/completed_orders', 'SafeBdMainController@CompletedOrder');
+  Route::get('/add_new_product', 'SafeBdMainController@AddNewProduct');
+});
 
 Route::prefix('category')->group(function () {
   Route::get('/safety', 'SafeBdMainController@Safety');
