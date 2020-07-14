a503cb2cf1461a316ca1f0e0b7a7a9b227eae7ec
Biswajit Biswas <jfbiswajit@gmail.com>
2020-07-14 10:07:19 +0600

    Add: All about styling:

--- resources/views/admin/add_new_product.blade.php
+++ resources/views/admin/add_new_product.blade.php
@@ -1,37 +1,40 @@
 @include('admin.layouts.header')
-  <div class="container p-4">
-    <form method="POST" action="{{ url('add_new_product') }}" class="p-5 border" enctype="multipart/form-data">
-		@csrf
-      <div class="row">
-      	<div class="col-md-6 form-group">
-      		<label>Product Name</label>
-      		<input type="text" class="form-control" required name="name">
-      	</div>
-      	<div class="col-md-6 form-group">
-      		<label for="">Description</label>
-      		<input type="text" class="form-control" required name="desc">
-      	</div>
-      	<div class="col-md-6 form-group">
-      		<label for="inputState">Category</label>
-      		<select id="inputState" class="form-control" required name="id">
-      			<option selected>Choose...</option>
-      			@foreach ($categories as $key=>$item)
+  <div class="container p-5">
+		<div class="border rounded">
+			<h4 class="bg-light p-3 text-muted">Add New Product</h4>
+			<form class="p-3" method="POST" action="{{ url('add_new_product') }}" enctype="multipart/form-data">
+				@csrf
+				<div class="row">
+					<div class="col-md-6 form-group">
+						<label>Product Name</label>
+						<input type="text" class="form-control" required name="name">
+					</div>
+					<div class="col-md-6 form-group">
+						<label for="">Description</label>
+						<input type="text" class="form-control" required name="desc">
+					</div>
+					<div class="col-md-6 form-group">
+						<label for="inputState">Category</label>
+						<select id="inputState" class="form-control" required name="id">
+							<option selected>Choose...</option>
+							@foreach ($categories as $key=>$item)
 							<option value="{{ $key }}"> {{ $item }} </option>
-						@endforeach
-      		</select>
-      	</div>
-      	<div class="col-md-6 form-group">
-      		<label for="">Price</label>
-      		<input type="text" class="form-control" required name="price">
-      	</div>
-      	<div class="col-md-6 form-group">
-      		<label>Thumbnail</label>
-      		<input type="file" class="form-control-file" required name="thumb">
-      	</div>
-      	<div class="col-md-6 form-group">
-      		<button type="submit" class="btn btn-block btn-success mt-2">Add Product</button>
-      	</div>
-      </div>
-    </form>
+							@endforeach
+						</select>
+					</div>
+					<div class="col-md-6 form-group">
+						<label for="">Price</label>
+						<input type="text" class="form-control" required name="price">
+					</div>
+					<div class="col-md-6 form-group">
+						<label>Thumbnail</label>
+						<input type="file" class="form-control-file" required name="thumb">
+					</div>
+					<div class="col-md-6 form-group">
+						<button type="submit" class="btn btn-block btn-success mt-2">Add Product</button>
+					</div>
+				</div>
+			</form>
+		</div>
   </div>
 @include('admin.layouts.footer')
