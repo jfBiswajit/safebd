c6a2f5ba96444c358a08f73451e1fb8499300a0e
Biswajit Biswas <jfbiswajit@gmail.com>
2020-07-15 10:48:59 +0600

    Info: Completed

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -3,13 +3,13 @@
 	<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
 		<div class="carousel-inner">
 			<div class="carousel-item active">
-				<img class="d-block w-100" src="{{ asset('img/hero/banner-1.png') }}" alt="First slide">
+				<img class="d-block w-100" src="{{ asset('img/hero/banner-1.jpg') }}" alt="First slide">
 			</div>
 			<div class="carousel-item">
-				<img class="d-block w-100" src="{{ asset('img/hero/banner-2.png') }}" alt="Second slide">
+				<img class="d-block w-100" src="{{ asset('img/hero/banner-2.jpg') }}" alt="Second slide">
 			</div>
 			<div class="carousel-item">
-				<img class="d-block w-100" src="{{ asset('img/hero/banner-3.png') }}" alt="Third slide">
+				<img class="d-block w-100" src="{{ asset('img/hero/banner-3.jpg') }}" alt="Third slide">
 			</div>
 		</div>
 		<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
