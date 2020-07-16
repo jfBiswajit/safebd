ea19b199dad92257822feb3cfc9b706fe0454595
Biswajit Biswas <jfbiswajit@gmail.com>
2020-07-16 13:54:21 +0600

    Add: all about styling

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -9,7 +9,8 @@
 	<meta name="description" content="" />
 	<meta name="author" content="" />
 	<title>{{ $title }}</title>
-	<link rel="shortcut icon" href="https://www.iconarchive.com/download/i91959/icons8/windows-8/Users-Administrator.ico"
+	<link rel="shortcut icon"
+		href="https://cdn.iconscout.com/icon/free/png-256/key-keys-access-entry-lock-unlock-open-4-23086.png"
 		type="image/x-icon">
 	<link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
 	<link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet"
