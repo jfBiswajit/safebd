7005794e187a54e09c89ecc7f20e431d617f16fe
Biswajit Biswas <jfbiswajit@gmail.com>
2020-07-16 11:50:25 +0600

    Fix: strach

--- resources/views/admin/products.blade.php
+++ resources/views/admin/products.blade.php
@@ -3,7 +3,7 @@
 	<div class="card mb-4 mt-4">
 		<div class="card-header">
 			<i class="fas fa-table mr-1"></i>
-			Pending Orders
+			Update Products
 		</div>
 		<div class="card-body">
 			<div class="table-responsive">
