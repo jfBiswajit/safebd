54f38ae0b77eafa0cc519224bce2c47d88cd3dc1
Arif.Trinoq <asarifkhan08@gmail.com>
2020-08-13 11:44:47 -0600

    fix:RECOMENDED price updated

--- resources/views/users/product_details.blade.php
+++ resources/views/users/product_details.blade.php
@@ -57,7 +57,7 @@
 						<img class="card-img-top" src="/storage/{{ $product->thumb }}" alt="{{ $product->name }}">
 						<div class="card-body">
 							<p class="card-title text-capitalize font-weight-bold" style="font-size: 0.9rem">{{ $product->name }}</p>
-							<h5 class="float-right text-danger" style="font-size: 0.9rem"><b>৳ 250</b></h5>
+							<h5 class="float-right text-danger" style="font-size: 0.9rem"><b>{{ $product->price }}</b></h5>
 							<button href="#" class="btn btn-success add_to_cart font-weight-light" data-id="{{ $product->id }}"
 								data-name="{{ $product->name }}" data-price="{{ $product->price }}">Add To Cart</button>
 						</div>
